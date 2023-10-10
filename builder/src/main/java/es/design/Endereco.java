27acd799c504777b79fbaf8ffa436ffba056dc0f
soft-felipe <felipem791@gmail.com>
2023-10-09 21:34:40 -0300

    feat: adicionando atributo não nulo

--- builder/src/main/java/es/design/Endereco.java
+++ builder/src/main/java/es/design/Endereco.java
@@ -2,12 +2,15 @@ package es.design;
 
 import lombok.Builder;
 import lombok.Data;
+import lombok.NonNull;
 
 @Data
 @Builder
 public class Endereco {
 
+    @NonNull
     private String rua;
+
     private String setor;
     private String cep;
     private int numero;
